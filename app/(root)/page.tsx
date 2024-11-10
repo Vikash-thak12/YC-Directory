d365d5412dfdb5c13e44da3664c9cc5a1364f62a
Vikash Thakur <thakvikash622@gmail.com>
2024-11-10 18:42:34 +0530

    Installed: ShadCN UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1>Hey there how are you</h1>
+      <h1 className='heading'>Hey there how are you</h1>
     </div>
   )
 }
