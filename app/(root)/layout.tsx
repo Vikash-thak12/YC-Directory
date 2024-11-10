a2c6f6e91da8ae841cbe1b403a308b5840ea931c
Vikash Thakur <thakvikash622@gmail.com>
2024-11-10 18:25:52 +0530

    Update: Theme and Fonts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 export default function Layout({ children}: Readonly<{children: React.ReactNode}>){
     return (
-        <main>
+        <main className="font-work-sans">
             <Navbar />
             {children}
         </main>
