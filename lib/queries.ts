21e0cc0b366ed76c95495ddcc657b1533cf6adfc
Vikash Thakur <thakvikash622@gmail.com>
2024-11-12 22:13:41 +0530

    Minor changes

--- lib/queries.ts
+++ lib/queries.ts
@@ -13,3 +13,18 @@ export const startup_Query = defineQuery(`*[_type == 'startup' && defined(slug.c
     category,
     image
 }`)
+
+export const startup_By_Id_Queries = defineQuery(`*[_type == 'startup' && _id == $id][0]{
+    _id, 
+    title,
+    slug,
+    _createdAt,
+    author ->{
+      _id, name, username, image, bio
+    },
+    views, 
+    description,
+    category,
+    image,
+    pitch
+}`)
