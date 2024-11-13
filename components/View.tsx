bcdb64019acfcfba0778e89132122da76026c164
Vikash Thakur <thakvikash622@gmail.com>
2024-11-13 10:00:39 +0530

    Able to update the views after each page visit

--- components/View.tsx
+++ components/View.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { startup_View_Query } from '@/lib/queries'
+import { unstable_after as after } from 'next/server';
+import { writeClient } from '@/sanity/lib/writeClient';
 
 const View = async ({ id }: {id: string}) => {
     const { views: totalViews} = await client.withConfig({ useCdn: false}).fetch(startup_View_Query, { id })
+
+    after(() => {
+        writeClient
+          .patch(id)
+          .set({ views: totalViews + 1})
+          .commit()
+    })
   return (
     <div className='view-container'>
       <div className='absolute -top-2 -right-2'>
