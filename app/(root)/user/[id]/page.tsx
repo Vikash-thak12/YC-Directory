c7e629a693d9504586596139986a99604829b3cc
Vikash Thakur <thakvikash622@gmail.com>
2024-11-14 14:52:52 +0530

    Completed: Basic User Page

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,26 +1,48 @@
 import { auth } from '@/auth';
 import { client } from '@/sanity/lib/client';
 import { author_By_Id_Query } from '@/sanity/lib/queries';
-import React from 'react'
+import Image from 'next/image';
+import React, { Suspense } from 'react'
 
 const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
     const session = await auth();
 
     const user = await client.fetch(author_By_Id_Query, { id })
-    console.log("The user is:", user)
+    const { name, image, bio, username} = user;
     return (
         <>
-        page
-            {/* <section className='profile_container'>
-                <div className='profile_card'>
+            page
+            <section className='profile_container'>
+                <div className='profile_card !bg-gray-400'>
                     <div className='profile_title'>
-                        <h3>
-                            {user.name}
+                        <h3 className='text-24-black uppercase line-clamp-1 text-center'>
+                            {name}
                         </h3>
                     </div>
+                    <Image
+                        src={image}
+                        alt={name}
+                        width={220}
+                        height={220}
+                        className='profile_image'
+                    />
+                    <p className="text-30-extrabold mt-7 text-center">
+                        @{username}
+                    </p>
+                    <p className="mt-1 text-center text-14-normal">{bio}</p>
                 </div>
-            </section> */}
+                <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+                    <p className="text-30-bold">
+                        {session?.id === id ? "Your" : "All"} Startups
+                    </p>
+                    <ul className="card_grid-sm">
+                        <Suspense>
+                            {/* <UserStartups id={id} /> */}
+                        </Suspense>
+                    </ul>
+                </div>
+            </section>
         </>
     )
 }
